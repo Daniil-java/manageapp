af71616c9e804610270ad6c1e93deaa79641b762
DanielK <saponembigas@gmail.com>
2025-10-13 23:38:43 +0700

    feat(et-5): pom.xml & application.yaml & TelegramUserService & shared library: update.

--- src/main/java/com/kuklin/manageapp/common/library/tgutils/BotIdentifier.java
+++ src/main/java/com/kuklin/manageapp/common/library/tgutils/BotIdentifier.java
@@ -8,7 +8,12 @@ import lombok.RequiredArgsConstructor;
 public enum BotIdentifier {
     CALORIE_BOT("@track_your_work_bot"),
     ASSISTANT_BOT("@personal_calen_bot"),
-    KWORK("@kworker_parserbot");
+    KWORK("@kworker_parserbot"),
+    BOOKING_BOT("@slotmanage_bot"),
+    HH_BOT("@workhunttg_bot"),
+    POMIDORO_BOT("@taskGPT_Bot"),
+    AVIA_BOT("@deparr_bot")
+    ;
 
     private final String botUsername;
 }
