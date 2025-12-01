f4e91f5c7379771220f29dd4ae47e302ead59b1b
DanielK <saponembigas@gmail.com>
2025-12-01 17:48:46 +0700

    fix(et-37): Фикс бота помидоро

--- src/main/java/com/kuklin/manageapp/bots/pomidorotimer/telegram/PomidoroTelegramBot.java
+++ src/main/java/com/kuklin/manageapp/bots/pomidorotimer/telegram/PomidoroTelegramBot.java
@@ -4,11 +4,15 @@ import com.kuklin.manageapp.bots.pomidorotimer.configurations.TelegramPomidoroTi
 import com.kuklin.manageapp.common.library.tgmodels.TelegramBot;
 import com.kuklin.manageapp.common.library.tgutils.BotIdentifier;
 import com.kuklin.manageapp.common.services.AsyncService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
+@Slf4j
 public class PomidoroTelegramBot extends TelegramBot {
     @Autowired
     private PomidoroTelegramFacade pomidoroTelegramFacade;
@@ -20,7 +24,17 @@ public class PomidoroTelegramBot extends TelegramBot {
     }
     @Override
     public void onUpdateReceived(Update update) {
-        boolean result = doAsync(asyncService, update, u -> pomidoroTelegramFacade.handleUpdate(update));
+        boolean result = doAsync(asyncService, update, u ->  {
+            BotApiMethod method = pomidoroTelegramFacade.handleUpdate(update);
+
+            if (method != null) {
+                try {
+                    execute(method);
+                } catch (TelegramApiException e) {
+                    log.error("Pomidoro timer: telegram execute error!");
+                }
+            }
+        });
 
         if (!result) {
             notifyAlreadyInProcess(update);
