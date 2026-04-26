b508dc6e3eb393461083aa8d7555530744194eef
DanielK <saponembigas@gmail.com>
2026-04-26 21:56:16 +0700

    feat(et-68): fix smoking api

--- src/main/java/com/kuklin/manageapp/bots/nicotinebot/components/controllers/SmokingRecordsController.java
+++ src/main/java/com/kuklin/manageapp/bots/nicotinebot/components/controllers/SmokingRecordsController.java
@@ -15,24 +15,24 @@ public class SmokingRecordsController {
     private final SmokingRecordService smokingRecordService;
 
     @PostMapping("/{id}")
-    public SmokingRecord createSmokingRecord(@PathVariable Long id) {
+    public SmokingRecord createSmokingRecord(@PathVariable("id") Long id) {
         return smokingRecordService.create(id);
     }
 
     @GetMapping("/{id}")
-    public List<SmokingRecord> getByPeriod(@PathVariable Long id,
+    public List<SmokingRecord> getByPeriod(@PathVariable("id") Long id,
                                            @RequestParam Instant start,
                                            @RequestParam Instant end) {
         return smokingRecordService.getByPeriod(id, start, end);
     }
 
     @GetMapping("/{id}/last")
-    public SmokingRecord getLastSmokingRecordOrNull(@PathVariable Long id) {
+    public SmokingRecord getLastSmokingRecordOrNull(@PathVariable("id") Long id) {
         return smokingRecordService.getLastSmokingEvent(id).orElse(null);
     }
 
     @GetMapping("/{id}/all")
-    public List<SmokingRecord> gelAll(@PathVariable Long id) {
+    public List<SmokingRecord> gelAll(@PathVariable("id") Long id) {
         return smokingRecordService.getAllByUser(id);
     }
 
